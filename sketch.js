19db89f6d26fbd8e7a94a1289e353bac92f31285
Karl Sykes <33551959+KarlSykes@users.noreply.github.com>
2020-12-27 20:56:41 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -63,11 +63,8 @@ function draw() {
 //Remove words still in record that don't match the query
 function filterWordsRecords(){
       console.log("Records before filter", records);  // DEBUG
-      records = records.filter(colour => {
-            console.log("FILTER:", colour[1].substring(0, query.length).toLowerCase())
-            console.log(query.toLowerCase())
-            colour[1].substring(0, query.length).toLowerCase() == query.toLowerCase()
-      })
+      records = records.filter(colour => 
+            colour[1].substring(0, query.length).toLowerCase() == query.toLowerCase())
       console.log("Records after filter", records);  // DEBUG
 }
 
